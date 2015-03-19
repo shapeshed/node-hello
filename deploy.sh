09b982353a55186fe300e3b176c1c8222febce5c
George Ornbo <george@shapeshed.com>
2015-03-19 16:32:41 +0000

    [feature] add branches / environments

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,22 @@
 #! /bin/bash
 
 SHA1=$1
+BRANCH=$2
+
+case $BRANCH in
+  master)
+    $ENVIRONMENT_NAME=hello-env
+    ;;
+  staging)
+    $ENVIRONMENT_NAME=hello-staging
+    ;;
+  production)
+    $ENVIRONMENT_NAME=hello-production
+    ;;
+esac
 
 # Deploy image to Docker Hub
-sudo docker push shapeshed/node-hello:$SHA1
+sudo docker push shapeshed/node-hello:$SHA2
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=pebbledocker
@@ -15,5 +28,5 @@ aws elasticbeanstalk create-application-version --application-name hello \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env \
+aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT_NAME \
       --version-label $SHA1
